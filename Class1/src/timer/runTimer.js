2b009cc26007ab989f0a5cf4071729cf86930f69
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-13 23:17:36 -0800

    Fulfills all requirements for this file

--- Class1/src/timer/runTimer.js
+++ Class1/src/timer/runTimer.js
@@ -1,3 +1,8 @@
+/* Perform the following tasks:
+Examine the code and determine each script's behavior.
+Refactor the code to use let/const.
+*/
+
 import Timer from './Timer';
 
 const countdown = new Timer(10);
