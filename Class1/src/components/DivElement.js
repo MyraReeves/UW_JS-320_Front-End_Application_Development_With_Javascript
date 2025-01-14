871f12d6ce42c01d7bbfbb52f43323dca5292289
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-13 19:32:46 -0800

    Adds instructions specific to this file

--- Class1/src/components/DivElement.js
+++ Class1/src/components/DivElement.js
@@ -1,5 +1,22 @@
-// Import HTMLElement here
+/* INSTRUCTIONS:
+Create a module that meets the following requirements:
+  1. Create a class called 'DivElement' that extends the 'HTMLElement' class created previously.
+     You'll need to import the class made in that file.
+     The new class should have a constructor with one parameter: content
+     The new class should inherit the 'render' function in the parent class
+      (do not override it, aka define it in 'DivElement').
+  2. Within the 'render' function, use a template literal to render the element.
+See the README for an example.
+*/
 
-// Define class here
+// //////////////////////// //
+// Import HTMLElement here //
+// ////////////////////// //
 
-// Export class here
+// ////////////////// //
+// Define class here //
+// //////////////// //
+
+// ////////////////// //
+// Export class here //
+// //////////////// //
