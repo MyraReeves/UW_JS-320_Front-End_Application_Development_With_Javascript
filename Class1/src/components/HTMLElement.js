59b1be87d051221dc12b9111e350d2d32892760b
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-13 18:14:02 -0800

    Adds instructions specific to this file

--- Class1/src/components/HTMLElement.js
+++ Class1/src/components/HTMLElement.js
@@ -1,4 +1,25 @@
-// Define class here
+/* INSTRUCTIONS:
+  Create a module that meets the following requirements:
+    1. Create a class called `HTMLElement`.
+        This class should have a constructor with two parameters:  tag and content.
+          Both of these parameters should be stored as instance variables within the class.
+        Instance variables should have the same names as the parameters.
 
-// Export class here
+    2. Once the HTMLElement class is created, add a function to the class called `render`
+        It should be possible to call the render function publicly.
+        This function should return the HTML element with the content provided.
+        Within `render`, use a template literal to render the element.
+
+  See the README for an example.
+*/
+
+// /////////////////////
+// Define class here //
+// ///////////////////
+
+// /////////////////////////////////////////////
+// Export class here.                        //
+// Change what is below to export the class //
+// instead of an empty object literal.     //
+// /////////////////////////////////////////
 export default {};
