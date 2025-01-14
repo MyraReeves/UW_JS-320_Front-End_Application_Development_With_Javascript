8d584a73dc64bc0c190e65701b1ab5c301cfb0c5
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-13 23:16:18 -0800

    Fulfills all requirements for this file

--- Class1/src/timer/Timer.js
+++ Class1/src/timer/Timer.js
@@ -10,15 +10,15 @@ function Timer(seconds) {
   this.seconds = seconds;
 }
 
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
+Timer.prototype.start = () => {
+  // const instance = this;
+  const timerInterval = setInterval(() => {
+    if (this.seconds === 0) {
       clearInterval(timerInterval);
     }
 
-    console.log(instance.seconds);
-    instance.seconds -= 1;
+    console.log(this.seconds);
+    this.seconds -= 1;
   }, 1000);
 };
 
