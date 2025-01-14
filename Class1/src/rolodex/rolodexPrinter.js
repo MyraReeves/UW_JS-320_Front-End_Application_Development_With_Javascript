108b69c11d99e87797d666a1742f401ab2fd7581
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-14 14:40:49 -0800

    Refactors name array for object destructuring

--- Class1/src/rolodex/rolodexPrinter.js
+++ Class1/src/rolodex/rolodexPrinter.js
@@ -3,16 +3,25 @@
 Refactor the code to use template literals.
 Refactor the code to use arrow function(s).
 Refactor the code to use object destructuring.
+Refactor the code to use array destructuring.
 */
 
 import people from './people.json';
 
 people.forEach((person) => {
-  const names = person.name.split(' ');
-  const firstName = names[0];
-  const lastName = names[1];
+  const [firstName, lastName] = person.name.split(' ');
   const { email } = person;
   const { phone } = person;
 
   console.log(`First name: ${firstName} \nLast name: ${lastName} \nEmail: ${email} \nPhone number: ${phone} \n`);
 });
+
+/* It could also be refactored even shorter into:
+
+people.forEach(({ name, email, phone }) => {
+  const [firstName, lastName] = name.split(' ');
+
+  console.log(`First name: ${firstName} \nLast name: ${lastName} \nEmail: ${email} \nPhone
+  number: ${phone} \n`);
+});
+*/
