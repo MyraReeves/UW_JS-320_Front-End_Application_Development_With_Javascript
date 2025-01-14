f1132616213cf1058d51e50ee81d206d72afa5c6
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-13 21:44:53 -0800

    Adds instructions specific to this file

--- Class1/src/timer/Timer.js
+++ Class1/src/timer/Timer.js
@@ -1,3 +1,11 @@
+/* Perform the following tasks:
+Examine the code and determine each script's behavior.
+Refactor the code to use the ES6 class syntax.
+Refactor the code to use arrow function(s).
+Refactor the code to use let/const.
+Refactor the code to avoid assigning this to a variable.
+*/
+
 function Timer(seconds) {
   this.seconds = seconds;
 }
