9057fd1d48402ade24ce2c8273926fcf18ba3e16
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-17 23:30:38 -0800

    Returns header and horizontal rule line

---
+++ Class2/blog-homepage/src/homepage-app/MissedItApp.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+
+
+function MissedItApp() {
+  const header = "In case you missed it"
+
+  // const [image, setimages] = useState([''])
+  // const [title, settitles] = useState('')
+
+  return (
+    <>
+      <h2>{header}</h2>
+      <hr/>
+
+    </>
+  )
+}
+
+export default MissedItApp
