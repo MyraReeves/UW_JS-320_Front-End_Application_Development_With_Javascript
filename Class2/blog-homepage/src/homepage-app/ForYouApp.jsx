0ca0c06ec3a651dd84dbbac5baccbb6214171528
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-17 23:28:29 -0800

    Returns header and horizontal rule line

---
+++ Class2/blog-homepage/src/homepage-app/ForYouApp.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+
+
+function ForYouApp() {
+  const header = "For you"
+
+  // const [image, setimages] = useState([''])
+  // const [title, settitles] = useState('')
+
+  return (
+    <>
+      <h2>{header}</h2>
+      <hr/>
+
+    </>
+  )
+}
+
+export default ForYouApp
