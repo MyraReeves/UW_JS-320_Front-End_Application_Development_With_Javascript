6588785385c4fadfe473937f7b214f0e3dde2838
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-21 01:55:11 -0800

    Reorganizes elements

--- Class2/blog-homepage/src/homepage-app/ForYouArticle.jsx
+++ Class2/blog-homepage/src/homepage-app/ForYouArticle.jsx
@@ -1,16 +1,27 @@
-function ForYouArticle(props) {
+
+import Picture from './PreviewPicture'
+import AuthorPicture from "./AuthorPicture";
+
+function ForYouArticle() {
+  const articlePreview = 'https://placehold.co/402x519'
+  const articleAlt = 'Preview image of article'
+  const authorPic = 'https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png'
+  const authorAlt = 'Small round image of the author'
 
   return (
     <section className="for-you">
-      <Picture imgSrc={articlePreview} imgAlt={imgAlt} />
-      <div>"Audio available"</div>
+      <Picture imgSrc={articlePreview} imgAlt={articleAlt} />
+      <div>Audio available<br />
       <h2>Title</h2>
-      <div>Description</div>
-      <Picture imgSrc={authorPic} imgAlt={imgAlt} />
-      <div>authorName</div>
-      <div>Article date</div>
-      <div>Article length</div>
-      <Picture imgSrc={bookmark} imgAlt={imgAlt} />
+      Description
+        <div className="bottomOfDiv">
+          <AuthorPicture className="authorPic" imgSrc={authorPic} imgAlt={authorAlt} />
+          <h4>authorName</h4>
+          Article date •
+          Article length
+          {/* <Picture imgSrc={bookmark} imgAlt={imgAlt} /> */}
+        </div>
+      </div>
     </section>
   )
 }
