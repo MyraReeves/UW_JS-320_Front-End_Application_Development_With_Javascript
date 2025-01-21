106d926f8481b8abd2a4c046cbb4b0b368eb5795
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-21 01:56:42 -0800

    Replaces placeholder text with key/value pairs in json file instead

--- Class2/blog-homepage/src/homepage-app/ForYouArticle.jsx
+++ Class2/blog-homepage/src/homepage-app/ForYouArticle.jsx
@@ -1,29 +1,45 @@
-
+import PropTypes from "prop-types";
 import Picture from './PreviewPicture'
 import AuthorPicture from "./AuthorPicture";
+import BookmarkIcon from "./BookmarkIcon";
+import yourArticles from './your-articles.json'
 
-function ForYouArticle() {
-  const articlePreview = 'https://placehold.co/402x519'
+function ForYouArticle(props) {
+  // const {itemToAdd, setItemToAdd, setTodos, todos} = props;
+  // const articlePreview = 'https://placehold.co/402x519'
   const articleAlt = 'Preview image of article'
-  const authorPic = 'https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png'
+  // const authorPic = 'https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png'
   const authorAlt = 'Small round image of the author'
+  const bookmarkImage = 'https://cdn-icons-png.flaticon.com/512/494/494568.png'
+  const bookmarkAlt = 'Small icon of a bookmark'
 
   return (
     <section className="for-you">
-      <Picture imgSrc={articlePreview} imgAlt={articleAlt} />
-      <div>Audio available<br />
-      <h2>Title</h2>
-      Description
-        <div className="bottomOfDiv">
-          <AuthorPicture className="authorPic" imgSrc={authorPic} imgAlt={authorAlt} />
-          <h4>authorName</h4>
-          Article date •
-          Article length
-          {/* <Picture imgSrc={bookmark} imgAlt={imgAlt} /> */}
+      {yourArticles.map( (yourArticles, index) => (
+        <div className="eachArticle" key={index} style={{display: 'inline-block'}}>
+          <Picture imgSrc={yourArticles.image} imgAlt={articleAlt} />
+          <div className="articleInfo">
+            {/* <p>Audio available</p> */}
+            <h2>{yourArticles.title}</h2>
+            <p>{yourArticles.description}</p>
+            <div className="bottomOfDiv">
+              <AuthorPicture className="authorPic" imgSrc={yourArticles.author.image} imgAlt={authorAlt} />
+              <h4>{yourArticles.author.name}</h4>
+              <script>new Date({yourArticles.postedDate}).toDateString()</script> •
+              Article length {yourArticles.minutesToRead}
+              <BookmarkIcon imgSrc={bookmarkImage} imgAlt={bookmarkAlt} />
+            </div>
+          </div>
         </div>
-      </div>
+      ))
+      }
     </section>
   )
+}
+
+Picture.propTypes = {
+  imgSrc: PropTypes.string.isRequired,
+  imgAlt: PropTypes.string.isRequired,
 }
 
 export default ForYouArticle
