b3f3d5e2e903883af1d3decf8a6b8ff46fdd6b85
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-21 02:21:39 -0800

    Replaces placeholder text with key/value pairs of json file instead

--- Class2/blog-homepage/src/homepage-app/MissedItArticle.jsx
+++ Class2/blog-homepage/src/homepage-app/MissedItArticle.jsx
@@ -1,16 +1,37 @@
+// import PropTypes from "prop-types";
+import Picture from './PreviewPicture'
+import AuthorPicture from "./AuthorPicture";
+import BookmarkIcon from "./BookmarkIcon";
+import missedArticles from './missed-articles.json'
+
 function MissedItArticle(props) {
+  // const articlePreview = 'https://placehold.co/402x519'
+  const articleAlt = 'Preview image of article'
+  // const authorPic = 'https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png'
+  const authorAlt = 'Small round image of the author'
+  const bookmarkImage = 'https://cdn-icons-png.flaticon.com/512/494/494568.png'
+  const bookmarkAlt = 'Small icon of a bookmark'
 
   return (
-    <section className="for-you">
-      <Picture imgSrc={articlePreview} imgAlt={imgAlt} />
-      <div>"Member preview"</div>
-      <h2>Title</h2>
-      <div>Description</div>
-      <Picture imgSrc={authorPic} imgAlt={imgAlt} />
-      <div>authorName</div>
-      <div>Article date</div>
-      <div>Article length</div>
-      <Picture imgSrc={bookmark} imgAlt={imgAlt} />
+    <section className="missed-it">
+      {missedArticles.map( (missedArticles, index) => (
+        <div className="eachArticle" key={index} style={{display: 'inline-block'}}>
+          <Picture imgSrc={missedArticles.image} imgAlt={articleAlt} />
+          <div className="articleInfo">
+            {/* <p>Audio available</p> */}
+            <h2>{missedArticles.title}</h2>
+            <p>{missedArticles.description}</p>
+            <div className="bottomOfDiv">
+              <AuthorPicture className="authorPic" imgSrc={missedArticles.author.image} imgAlt={authorAlt} />
+              <h4>{missedArticles.author.name}</h4>
+              <script>new Date({missedArticles.postedDate}).toDateString()</script> •
+              Article length {missedArticles.minutesToRead}
+              <BookmarkIcon imgSrc={bookmarkImage} imgAlt={bookmarkAlt} />
+            </div>
+          </div>
+        </div>
+      ))
+      }
     </section>
   )
 }
