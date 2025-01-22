52c0af61b69f4ee094a825a8c1d5065852e00156
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-21 19:32:28 -0800

    Checks for 'Audio Available' conditional and displays that paragraph if present

--- Class2/blog-homepage/src/homepage-app/ForYouArticle.jsx
+++ Class2/blog-homepage/src/homepage-app/ForYouArticle.jsx
@@ -1,14 +1,12 @@
 import PropTypes from "prop-types";
-import Picture from './PreviewPicture'
+import Picture from './PreviewPicture';
 import AuthorPicture from "./AuthorPicture";
 import BookmarkIcon from "./BookmarkIcon";
-import yourArticles from './your-articles.json'
+import yourArticles from './your-articles.json';
+import AudioAvailable from "./AudioAvailable";
 
 function ForYouArticle(props) {
-  // const {itemToAdd, setItemToAdd, setTodos, todos} = props;
-  // const articlePreview = 'https://placehold.co/402x519'
   const articleAlt = 'Preview image of article'
-  // const authorPic = 'https://www.kindpng.com/picc/m/722-7221920_placeholder-profile-image-placeholder-png-transparent-png.png'
   const authorAlt = 'Small round image of the author'
   const bookmarkImage = 'https://cdn-icons-png.flaticon.com/512/494/494568.png'
   const bookmarkAlt = 'Small icon of a bookmark'
@@ -19,7 +17,9 @@ function ForYouArticle(props) {
         <div className="eachArticle" key={index} style={{display: 'inline-block'}}>
           <Picture imgSrc={yourArticles.image} imgAlt={articleAlt} />
           <div className="articleInfo">
-            {/* <p>Audio available</p> */}
+
+            <AudioAvailable booleanValue={yourArticles.hasAudioAvailable}/>
+
             <h2>{yourArticles.title}</h2>
             <p>{yourArticles.description}</p>
             <div className="bottomOfDiv">
@@ -40,6 +40,7 @@ function ForYouArticle(props) {
 Picture.propTypes = {
   imgSrc: PropTypes.string.isRequired,
   imgAlt: PropTypes.string.isRequired,
+  booleanValue: PropTypes.bool.isRequired,
 }
 
 export default ForYouArticle
