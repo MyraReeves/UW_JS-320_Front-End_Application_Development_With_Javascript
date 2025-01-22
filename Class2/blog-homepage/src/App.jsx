4fae3dfa5d098e8a15f8dd80bba70b0a934af1d0
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-21 21:25:40 -0800

    Removes unnecessary import

--- Class2/blog-homepage/src/App.jsx
+++ Class2/blog-homepage/src/App.jsx
@@ -1,18 +1,15 @@
-import { useState } from 'react'
-
 import ForYouApp from './homepage-app/ForYouApp'
 import MissedItApp from './homepage-app/MissedItApp'
 
 import './App.css'
 
 function App() {
-
   return (
     <>
       <div>
         <ForYouApp />
       </div>
-      <div>
+      <div className='bottom-third'>
         <MissedItApp/>
       </div>
     </>
