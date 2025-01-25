d6fb38fb04db62ba0491d291c3a40feab78bf16b
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-24 23:38:43 -0800

    Initial commit

---
+++ Class3/ecommerce-ui/src/components/ShoppingCart.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+function ShoppingCart() {
+
+  return (
+    <>
+      <section className="shopping-cart">
+        <h3>Shopping Cart</h3>
+      </section>
+
+    </>
+  )
+}
+
+export default ShoppingCart;
