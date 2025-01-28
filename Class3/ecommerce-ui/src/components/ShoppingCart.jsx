a231468409400174287b8037a12dfea7bca79abf
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-28 01:14:51 -0800

    Displays contents of shopping cart

--- Class3/ecommerce-ui/src/components/ShoppingCart.jsx
+++ Class3/ecommerce-ui/src/components/ShoppingCart.jsx
@@ -1,15 +1,33 @@
+// import {useState} from "react"
 import PropTypes from "prop-types";
 
-function ShoppingCart() {
+function ShoppingCart(props) {
+  const {allRentals, costOfRental} = props
+  // const [allRentals, setAllRentals] = useState([])
 
   return (
     <>
       <section className="shopping-cart">
         <h3>Shopping Cart</h3>
+        <ul>
+          {allRentals.map((rentalToBePurchased, index) => {
+            return <li key={index}>
+                      <span className="align-left">{rentalToBePurchased}</span>
+                      <span className="align-right">{costOfRental}</span>
+                      <hr/>
+                    </li>
+          })}
+        </ul>
+        <span className ="total-due">Total Payment Due:</span>
       </section>
 
     </>
   )
+}
+
+ShoppingCart.propTypes = {
+  allRentals: PropTypes.arrayOf(PropTypes.string),
+  costOfRental: PropTypes.arrayOf(PropTypes.number)
 }
 
 export default ShoppingCart;
