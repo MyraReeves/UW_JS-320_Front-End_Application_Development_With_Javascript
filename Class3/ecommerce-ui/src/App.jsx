b076f2512064545968900d70a39f141a8291f630
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-29 21:26:29 -0800

    Sends allPrices array to ShoppingCart

--- Class3/ecommerce-ui/src/App.jsx
+++ Class3/ecommerce-ui/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <ShoppingCart
         allRentals = {allRentals}
-        setAllRentals = {setAllRentals}
+        allPrices = {allPrices}
       />
       <RentalsApp
         rentalToBePurchased = {rentalToBePurchased}
