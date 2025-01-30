4c0e0aa3bef149193291b2653e46b27c4f139fd9
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-01-29 22:39:32 -0800

    Separates mapping of allRentals and allPrices into their own functions

--- Class3/ecommerce-ui/src/components/ShoppingCart.jsx
+++ Class3/ecommerce-ui/src/components/ShoppingCart.jsx
@@ -1,22 +1,40 @@
 import PropTypes from "prop-types";
 
 function ShoppingCart(props) {
-  const {allRentals, costOfRental} = props
+  const {allRentals, allPrices} = props;
 
-  // const costOfRental = bnbProperties.payment.cost
+  const eachRental = () => {
+    allRentals.map((rentalToBePurchased, index) => {
+      return <span key={index} className="align-left">{rentalToBePurchased}</span>
+    })
+  };
+
+  const priceOfEach = () => {
+    allPrices.map( (costOfRental, index) => {
+      return <span key={index} className="align-right">{costOfRental}</span>
+    })
+  }
 
   return (
     <>
       <section className="shopping-cart">
         <h3>Shopping Cart</h3>
         <ul>
-          {allRentals.map((rentalToBePurchased, index) => {
+          <li>
+            {eachRental()} {priceOfEach()}
+            <hr/>
+          </li>
+          {/* {allRentals.map((rentalToBePurchased, index) => {
             return <li key={index}>
                       <span className="align-left">{rentalToBePurchased}</span>
+                    </li>
+          })} */}
+          {/* {allPrices.map((costOfRental, index) => {
+            return <li key={index}>
                       <span className="align-right">{costOfRental}</span>
                       <hr/>
                     </li>
-          })}
+          })} */}
         </ul>
         <div className ="total-due">Total Payment Due:
           <h2>$</h2>
@@ -30,7 +48,7 @@ function ShoppingCart(props) {
 
 ShoppingCart.propTypes = {
   allRentals: PropTypes.arrayOf(PropTypes.string),
-  costOfRental: PropTypes.arrayOf(PropTypes.number)
+  allPrices: PropTypes.arrayOf(PropTypes.number)
 }
 
 export default ShoppingCart;
