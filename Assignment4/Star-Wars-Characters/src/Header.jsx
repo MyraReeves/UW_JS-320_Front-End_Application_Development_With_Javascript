0455d7f48b35ce0147573f1706dd8d77f3ccad0d
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-07 00:42:01 -0800

    Adds the words 'Character Info' to the righthand side of the header

--- Assignment4/Star-Wars-Characters/src/Header.jsx
+++ Assignment4/Star-Wars-Characters/src/Header.jsx
@@ -1,11 +1,12 @@
 import logo from './images/header-title1.jpg'
 import headerImage from './images/header-image.jpg'
+import title from './images/title2.png'
 
 function Header() {
 
     return (
     <header>
-        <img src={logo} alt="Star Wars logo" /> <img src={headerImage} alt="header image" />
+        <img src={logo} alt="Star Wars logo"/><img src={headerImage} alt="header image"/>  <img src={title} alt='The words "Character Info"' className='title'/>
     </header>
     )
   }
