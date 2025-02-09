f7c2013f27044c8e0b1ee72960705c8a994687e6
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-08 22:49:21 -0800

    Adds onClick navigation to character cards

--- Assignment4/Star-Wars-Characters/src/Homepage.jsx
+++ Assignment4/Star-Wars-Characters/src/Homepage.jsx
@@ -1,12 +1,15 @@
+import { useNavigate } from 'react-router-dom'
 import characters from './character-info.json'
 
 function Homepage() {
+  const navigate = useNavigate()
 
 
   return (
     <div className='character-index'>
       {characters.map( (characters, index) => (
-        <div className="linkToEachCharacter" key={index} style={{display: 'inline-block'}}>
+
+        <div className="linkToEachCharacter" key={index} style={{display: 'inline-block'}} onClick={() => navigate(characters.page)}>
 
           <img className="previewPic" src={characters.image} alt={`Small photo of ${characters.name}`} />
           <h2>{characters.name}</h2>
