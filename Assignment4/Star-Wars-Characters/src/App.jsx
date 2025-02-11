9a723ff9f568ba6df7b9e456fff03869f012200d
Myra <163362998+MyraReeves@users.noreply.github.com>
2025-02-10 23:49:18 -0800

    Cleans up code

--- Assignment4/Star-Wars-Characters/src/App.jsx
+++ Assignment4/Star-Wars-Characters/src/App.jsx
@@ -1,29 +1,13 @@
-import { useNavigate } from 'react-router-dom'
-
 import './App.css'
 import RoutePaths from './RoutePaths';
 import Header from './Header'
 
 function App() {
 
-  // Move these into a different file so that you can reference it and map thru the array of address objects here
-  const navigate = useNavigate();
-
-  const navigateLuke = () => {
-    useNavigate('/luke')
-  };
-
-  const navigateR2 = () => {
-    navigate('/r2d2')
-  };
-
-
-
   return (
     <>
-      <RoutePaths />
       <Header />
-
+      <RoutePaths />
     </>
   )
 }
